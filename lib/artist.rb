09ef8ffe52df14a6883741ffb08b208355a89e40
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-25 19:45:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,24 @@
 require 'pry'
 class Artist 
   
+<<<<<<< HEAD
   attr_accessor :name
   @@all = 3
   
   def initialize(name)
     @name = name
+=======
+  attr_accessor :artist
+  
+  def initialize(name)
+    @artist = name
+>>>>>>> c5514c10a752129562637f85c53ffe97ad90fa6d
     @songs = []
   end
   
   def add_song(song)
     @songs << song
+<<<<<<< HEAD
     song.artist = self
 
   end
@@ -26,8 +34,23 @@ class Artist
   
   def self.song_count
     @@all
+=======
+    
+  end
+  
+  def songs 
+    @songs 
+>>>>>>> c5514c10a752129562637f85c53ffe97ad90fa6d
   end
     
   
   
 end
+<<<<<<< HEAD
+=======
+
+
+adele = Artist.new("Adele")
+adele.add_song("Hello")
+binding.pry
+>>>>>>> c5514c10a752129562637f85c53ffe97ad90fa6d
